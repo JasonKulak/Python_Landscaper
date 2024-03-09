86636a09cdeb3014bc20a45d32789e2a2ec2ad02
Jason Kulak <JasonKulak@yahoo.com>
2024-03-08 21:56:22 -0600

    Welcome message

--- landscaper.py
+++ landscaper.py
@@ -4,4 +4,23 @@ tools = [
     {"name": "your teeth", "revenue": 1, "cost": 0}
 ]
 
-print(tools)
+## What do the player(s) need to win?
+player = {
+    "money": 0,
+    "tool": 0,
+    "won_game": False
+}
+
+## What happens when I mow a lawn? need a "function to mow lawn"
+# def mow_lawn():
+
+
+## What happens when I upgrade my tools?
+## What happens when I  run out of tools? (if statement)
+## Do I have enough money?
+
+
+## Make an "alert" introducing the game
+    
+# Welcome message
+print("Welcome to Landscaper!!!")
